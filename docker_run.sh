36d130addd6c042046ee3c28839651b0e31c5fa1
vgaabor <vajda.gaabor@freemail.hu>
2021-03-10 22:34:46 +0100

    Javítás az cont sh-ban

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -dit --network host --name $1gep -h $1gep ubunmin:18.04 bash
-docker cp cont_start.sh simongep:/
+docker cp cont_start.sh $1gep:/
 docker exec $1gep /bin/bash /cont_start.sh $1 $2 $3
 docker exec $1gep rm /cont_start.sh
