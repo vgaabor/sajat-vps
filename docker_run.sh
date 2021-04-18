a0ca217bace0359292a64b9c6fbe9d679d241549
vgaabor <vajda.gaabor@freemail.hu>
2021-04-18 11:01:18 +0200

    Nem host net

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 1: user; 2: passw; 3: port
 
-docker run -dit --network host --name $1gep -h $1gep ubunmin:18.04 bash
+mkdir -p ../vol/$1gep
+
+docker run -dit -p $3:$3 --name $1gep -h $1gep -v $(pwd)/../vol/$1gep:/shared_folder_to_host ubunmin:18.04 bash
 docker cp cont_start.sh $1gep:/
 docker exec $1gep /bin/bash /cont_start.sh $1 $2 $3
 docker exec $1gep rm /cont_start.sh
